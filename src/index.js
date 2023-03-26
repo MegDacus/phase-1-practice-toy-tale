a3761e8b5bb0767cd411e07b17232ffdacb400f5
Megan Dacus <meganjustice0609@gmail.com>
2023-03-26 18:02:49 +1100

    Assignment Completed

--- src/index.js
+++ src/index.js
@@ -12,4 +12,70 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
-});
+
+  fetch('http://localhost:3000/toys')
+    .then (resp => resp.json())
+    .then (json => {
+    json.forEach ((toy) => {
+      let card = document.createElement('div');
+      let toyCollection = document.querySelector('#toy-collection');
+      let h2 = document.createElement('h2')
+      let img = document.createElement('img')
+      let p = document.createElement('p')
+      let button = document.createElement('button')
+      let likes = toy.likes;
+
+      button.addEventListener('click', (e) => {
+        let id = e.target.id;
+        console.log(e.target.id)
+        likes++;
+        p.textContent = (`${likes} likes!`)
+        
+        fetch(`http://localhost:3000/toys/${id}`, {
+        method: 'PATCH',
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+        body: JSON.stringify({
+          "likes": likes
+        })
+    })
+      
+    })
+      card.append(h2, img, p, button);
+      h2.innerHTML = toy.name;
+      img.classList.add('toy-avatar');
+      img.src = toy.image;
+      button.innerText = "Like ❤️"
+      button.classList.add('like-btn')
+      button.setAttribute('id', toy.id)
+      p.textContent = (`${toy.likes} likes!`)
+
+      card.classList.add('card');
+      toyCollection.appendChild(card);
+    })}
+    )
+    })
+
+  let submit = document.querySelector('#submit');
+  
+  submit.addEventListener('click', (e) => {
+    
+  let newName = document.getElementById('newName');
+  let newImage = document.getElementById('newImage');
+    
+    fetch ('http://localhost:3000/toys', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({
+        "name": newName.value,
+        "image": newImage.value,
+        "likes": 0
+      })
+    })
+  })
+  
